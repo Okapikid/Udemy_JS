78db9fc5b68c742541fd2596fb93a86dd6bfcd6d
Auggie <miaugus@gmail.com>
2022-07-15 10:03:50 -0400

    practice JS

--- complete-javascript-course-master/02-Fundamentals-Part-2/TEST/script.js
+++ complete-javascript-course-master/02-Fundamentals-Part-2/TEST/script.js
@@ -180,28 +180,48 @@
 //   }
 // }
 
-const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
-const tips = [];
-const totals = [];
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-}
+// const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+// const tips = [];
+// const totals = [];
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// }
 
-for (var i = 0; i < bills.length; i++) {
-  const tip = calcTip(bills[i]);
-  tips.push(tip);
-  totals.push(tip + bills[i]);
-}
-console.log(bills);
-console.log(tips);
-console.log(totals);
+// for (var i = 0; i < bills.length; i++) {
+//   const tip = calcTip(bills[i]);
+//   tips.push(tip);
+//   totals.push(tip + bills[i]);
+// }
+// console.log(bills);
+// console.log(tips);
+// console.log(totals);
 
-const calcAverage = function (arr) {
-  let sum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    sum += arr[i];
-  }
-  return sum / arr.length;
-}
-console.log(calcAverage(totals));
-console.log(calcAverage(tips));
+// const calcAverage = function (arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i++) {
+//     sum += arr[i];
+//   }
+//   return sum / arr.length;
+// }
+// console.log(calcAverage(totals));
+// console.log(calcAverage(tips));
+
+// PASSWORD CHECKER
+// const isThisAValidPassword = password => {
+//   let upper = false;
+//   let lower = false;
+//   if (password.length < 8) {
+//     return false;
+//   }
+//   for (let i = 0; i < password.length; i++) {
+//     if (password[i].toLowerCase() === password[i]) {
+//       lower = true;
+//     }
+//     if (password[i].toUpperCase() === password[i]) {
+//       upper = true;
+//     }
+//   }
+//   return lower && upper
+// }
+// console.log(isThisAValidPassword('Doggie'));
+// console.log(isThisAValidPassword('goodDoggo1'));
