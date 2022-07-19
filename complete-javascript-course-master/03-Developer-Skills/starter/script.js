ac0a747e4179cf6a6cfb4f06acb1d302a57a8e3d
Auggie <miaugus@gmail.com>
2022-07-19 09:59:57 -0400

    guess my number completed

--- complete-javascript-course-master/03-Developer-Skills/starter/script.js
+++ complete-javascript-course-master/03-Developer-Skills/starter/script.js
@@ -3,23 +3,50 @@
 
 // const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 
-const calcTempAmplitudeNew = function (temps1, temps2) {
-  const temps = temps1.concat(temps2);
-
-  let max = temps[0];
-  let min = temps[0];
-
-  for (let i = 0; i < temps.length; i++) {
-    const curTemp = temps[i];
-    if (typeof curTemp !== 'number') continue;
-    if (curTemp > max) max = curTemp;
-    if (curTemp < min) min = curTemp;
-  }
-  console.log(max);
-  console.log(min);
-  return max - min;
-};
-const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
-console.log(amplitudeNew);
-
 // PROBLEM 2: Function should now receive two arrays of temperatures
+
+// const calcTempAmplitudeNew = function (temps1, temps2) {
+//   const temps = temps1.concat(temps2);
+
+//   let max = temps[0];
+//   let min = temps[0];
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max);
+//   console.log(min);
+//   return max - min;
+// };
+// const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]);
+// console.log(amplitudeNew);
+
+// // PROBLEM 2: Function should now receive two arrays of temperatures
+
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: 'temp',
+//     unit: 'celcius',
+//     value: Number(prompt('Degrees celsius')),
+//   };
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+// console.log(measureKelvin());
+
+// const data1 = [17, 21, 23];
+// const data2 = [12, 5, -5, 0, 4];
+
+// const printForecast = function (arr) {
+//   let str = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     str += ` ${arr[i]}ºC in ${i + 1} days ...`;
+//   }
+//   console.log(`The temperature will be` + str);
+// };
+
+// printForecast(data1);
+// printForecast(data2);
--- complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,3 +1,50 @@
 'use strict';
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 
-console.log(document.querySelector(".message"));
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  // Response if no guess is input when checked
+  if (!guess) {
+    document.querySelector(
+      '.message'
+    ).textContent = `Please select a number before clicking "check"`;
+    // Response if the guess is too low
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = `🥶 Too low! 🥶`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = `😵 YOU FOOL! 😵`;
+      document.querySelector('.score').textContent = 0;
+    }
+    // Response if the guess is too high
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = `📈 Too high! 📈`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = `😵 YOU FOOL! 😵`;
+      document.querySelector('.score').textContent = 0;
+    }
+    // Response when guess is correct
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = `🥳 NAILED IT! 🥳`;
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.highscore').textContent = score;
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.highscore').textContent = 0;
+  document.querySelector('.guess').value = '';
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+});
